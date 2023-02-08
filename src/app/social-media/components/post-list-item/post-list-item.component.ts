50454c5fe4ac41e11cbc2be0190fa861975c277f
AmandineWA <amandine.monardo@web-atrio.com>
2023-02-08 16:00:12 +0100

    [AMM] resolver et output

--- src/app/social-media/components/post-list-item/post-list-item.component.ts
+++ src/app/social-media/components/post-list-item/post-list-item.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Post} from "../../models/post.model";
 
 @Component({
   selector: 'app-post-list-item',
   templateUrl: './post-list-item.component.html',
   styleUrls: ['./post-list-item.component.scss']
 })
-export class PostListItemComponent {
+export class PostListItemComponent implements OnInit{
 
+  @Input() post!: Post;
+  @Output() postCommented = new EventEmitter<{comment:string, postId: number}>()
+  constructor() {
+  }
+  ngOnInit() {
+  }
+
+  onNewComment(comment: string) {
+    this.postCommented.emit({comment, postId:this.post.id});
+  }
 }
