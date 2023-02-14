e5ca151dce194eb9d158f40ed600c30c2af2afab
AmandineWA <amandine.monardo@web-atrio.com>
2023-02-14 20:36:50 +0100

    [AMM] modification pessimiste et optimiste

--- src/app/reactive-state/components/single-candidate/single-candidate.component.ts
+++ src/app/reactive-state/components/single-candidate/single-candidate.component.ts
@@ -1,5 +1,5 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
-import {Observable, switchMap} from "rxjs";
+import {Observable, switchMap, take, tap} from "rxjs";
 import {Candidate} from "../../models/candidate.model";
 import {CandidatesService} from "../../services/candidates.service";
 import {ActivatedRoute, Router} from "@angular/router";
@@ -33,11 +33,28 @@ export class SingleCandidateComponent implements OnInit{
   }
 
   onHire() {
-
+    // Ajout 'optimiste' on met d'abord à jours les données de l'application avant même d'envoyer la requête au serveur
+    this.candidate$.pipe(
+      take(1),
+      tap(candidate => {
+        this.candidatesService.hireCandidate(candidate.id);
+        this.onGoBack();
+      })
+    ).subscribe();
   }
 
   onRefuse() {
-
+    // suppression 'pessimiste' on attend que la requête réussisse avant de metre à jours les données cotés application
+    // on récupère les data du candidat (l'observable)
+    // On utilise take() pour que la logique soit éxécutée une seul fois
+    // on déclenche la suppression (grace à l'id) et on redirige.
+    this.candidate$.pipe(
+      take(1),
+      tap(candidate => {
+        this.candidatesService.refuseCandidate(candidate.id);
+        this.onGoBack();
+      })
+    ).subscribe();
   }
 
   onGoBack() {
--- src/app/reactive-state/services/candidates.service.ts
+++ src/app/reactive-state/services/candidates.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
-import {BehaviorSubject, delay, map, Observable, tap} from "rxjs";
+import {BehaviorSubject, delay, map, Observable, switchMap, take, tap} from "rxjs";
 import {Candidate} from "../models/candidate.model";
 import {environment} from "../../../environments/environments";
 
@@ -47,5 +47,38 @@ export class CandidatesService {
     return this.candidates$.pipe(
       map(candidates => candidates.filter(candidate => candidate.id === id)[0])
     )
+  }
+
+  refuseCandidate(id: number):void{
+    // Modification pessimiste
+    this.setLoadingStatus(true);
+    this.http.delete(`${environment.apiUrl}/candidates/${id}`).pipe(
+      delay(1000), // simulation de la reponse sur serveur
+      switchMap(() => this.candidates$),
+      take(1),
+      map(candidates => candidates.filter(candidate => candidate.id !== id)),
+      tap(candidates => {
+        this._candidates$.next(candidates);
+        this.setLoadingStatus(false);
+      })
+    ).subscribe();
+  }
+
+  hireCandidate(id: number):void {
+    // modification optimiste
+    this.candidates$.pipe(
+      take(1),
+      map(candidates => candidates
+        .map(candidate => candidate.id === id ?
+          {...candidate, company: 'CaptnFaceSnap ltd'} :
+          candidate
+        )
+      ),
+      tap(updatedCandidates => this._candidates$.next(updatedCandidates)),
+      switchMap(updatedCandidates =>
+        this.http.patch(`${environment.apiUrl}/candidates/${id}`,
+          updatedCandidates.find(candidate => candidate.id))
+      )
+    ).subscribe()
   }
 }
